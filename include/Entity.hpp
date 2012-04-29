ad8fd24a7dc61754ac3a20c3b7a94b2ea339b171
Leo Walsh <leo.benjamin.walsh@gmail.com>
2012-04-29 17:36:48 +0100

    added gpl v3 headers to all files in the project.

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -1,9 +1,26 @@
 /*
- * Entity.h
- *
- *  Created on: Apr 6, 2012
- *      Author: leo
- */
+=============================================================================
+
+BreakoutClone GPL Source Code
+Copyright (C) 2012 Leo Walsh
+
+This file is part of the BreakoutClone GPL Source Code.  
+
+BreakoutClone Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BreakoutClone Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BreakoutClone Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+==============================================================================
+*/
 
 #ifndef ENTITY_HPP_
 #define ENTITY_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 /*
- * main.cpp
- *
- *  Created on: Apr 5, 2012
- *      Author: leo
- */
+=============================================================================
+
+BreakoutClone GPL Source Code
+Copyright (C) 2012 Leo Walsh
+
+This file is part of the BreakoutClone GPL Source Code.  
+
+BreakoutClone Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BreakoutClone Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BreakoutClone Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+==============================================================================
+*/
 
 #include <SFML/Graphics.hpp>
 #include "../include/Ball.hpp"
